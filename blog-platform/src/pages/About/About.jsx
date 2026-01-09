70ef81eceff58588e4fa15aeaa17600d7b14a041
olha-makarchuk <olamacarchuk@gmail.com>
2026-01-09 17:19:33 +0200

    add about and login pages

---
+++ blog-platform/src/pages/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className='about-container'>
+      Інформація про блог
+    </div>
+  )
+}
+
+export default About;
---
+++ blog-platform/src/pages/Auth/Login.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import "./Login.css";
+
+function Login() {
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/dashboard", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const from = location.state?.from?.pathname || "/dashboard";
+
+    login(email, password);
+
+    navigate(from, { replace: true });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="login-form">
+      <h2>Вхід в особистий кабінет</h2>
+
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Пароль:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+
+      <button type="submit">Увійти</button>
+    </form>
+  );
+}
+
+export default Login;
